f4207eac7cdf572af77485348f421d44efe05275
xqwzts251 <xqwzts251@gmail.com>
2015-10-24 14:45:59 +0200

    feat dialog and button

--- AccidentDialog.js
+++ AccidentDialog.js
@@ -1,8 +1,21 @@
 function AccidentDialog(){
+	this.element = document.getElementById('accidentDialog');
+	this.textContainer = document.createElement('div');
+	this.whatRow = document.createElement('div');
+	this.whereRow = document.createElement('div');
+	this.infoRow = document.createElement('div');
 
+	this.textContainer.appendChild(this.infoRow);
+	this.textContainer.appendChild(this.whereRow);
+	this.textContainer.appendChild(this.whatRow);
+	this.element.appendChild(this.textContainer);
 }
 
 AccidentDialog.prototype.show = function(accident){
-
+	this.element.style.display = "block";
+	//Renderowanie elementow
+	this.whatRow.textContent = "WHAT: " + accident.type;
+	this.whereRow.textContent = "WHERE: " + accident.x + " " + accident.y;
+	//infoRow...
 }
 
