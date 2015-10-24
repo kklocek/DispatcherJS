23627ec9a97c35d79390a4ec3d51db1eda457fe2
Abadd0n <lpytlak27@gmail.com>
2015-10-24 18:20:28 +0200

    feat: kolejne funkcjonalnosci w UnitDialog.js

--- UnitDialog.js
+++ UnitDialog.js
@@ -1,8 +1,33 @@
 function UnitDialog(){
+	this.name = document.getElementById('UnitDialog');
+	this.textContainer = document.createElement('div');
+	this.energyBar = document.createElement('div');
+	this.organizationBar = document.createElement('div');
+	this.skills = document.createElement('div');;
+	this.cars = document.createElement('div');;
 
+	this.textContainer.appendChild(this.energyBar);
+	this.textContainer.appendChild(this.organizationBar);
+	this.textContainer.appendChild(this.skills);
+	this.textContainer.appendChild(this.cars);
+	this.name.appendChild(this.textContainer);
 }
 
 UnitDialog.prototype.show = function(unit){
+	this.name.style.display = "block";
+	//Renderowanie elementow
+	this.energyBar.textContent = "ENERGY: " + unit.type;
+	this.organizationBar.textContent = "ORGANIZATION: " + unit.organization;
+	for(var i=0; i<unit.skills.length; i++){
+			var x = document.createElement('img');
+			x.href = unit.skills[i].imagePath;
+			//this.skills.textContent = "SKILLS: " + 
+			this.skills.appendChild(x);
 
-}
+	}
+	for(var j=0; i<unit.cars.length; i++){
+			var y = document.createElement('img');
+			y.href = unit.cars[i].imagePath;
+			this.cars.appendChild(y);
 
+	}
